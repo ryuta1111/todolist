1d1adad2016307f1054f17bca7c087e752fc5d6a
ryu <ryuta.lacrosse.soccer@gmail.com>
2024-06-02 19:06:04 +0900

    作業終了

---
+++ database/seeders/TodoListSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; //追記
+
+use function PHPSTORM_META\map;
+
+class TodoListSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+    DB::table('todo_lists')->insert(
+        [
+            [
+                'name' => 'テスト1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'テスト2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'テスト3',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]
+    );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoListController; //追記
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('アドレス , [コントローラー名::class , メソッド名]);
+Route::get('/list' , [TodoListController::class,'index']);
